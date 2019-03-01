73d480d2d72c5e74c0d9e942a7e34009fc46f116
Veis <2512668505@qq.com>
2019-03-02 01:35:43 +0800

    gojs

---
+++ gojs/src/main/java/com/example/demo/GojsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GojsdemoApplication.class, args);
+	}
+
+}
