b2ecac3454d7c236eae9c40492d440088c74bdf6
weikx <1246653289@qq.com>
2019-06-03 23:18:15 +0800

    案例

---
+++ demo1/src/main/java/com/kx/FjifenApplication.java
@@ -0,0 +1,13 @@
+package com.kx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FjifenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FjifenApplication.class, args);
+	}
+
+}
